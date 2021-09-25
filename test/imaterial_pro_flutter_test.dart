aca59e9192b202096b04a70451b7b2f73acd73dc
Mastersam <abadasamuelosp@gmail.com>
2021-09-25 03:03:39 -0700

    :test_tube: more tests
    more tests

--- test/imaterial_pro_flutter_test.dart
+++ test/imaterial_pro_flutter_test.dart
@@ -1,8 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:imaterial_pro_flutter/imaterial_pro_flutter.dart';
+import 'package:imaterial_pro_flutter/src/imaterial_icon_data.dart';
 
 void main() {
+  test('Test that regular icons are generated', () {
+    IMaterialIconRegularData icon = const IMaterialIconRegularData(0xe900);
+    expect(icon.codePoint, 0xe900);
+  });
+
+  test('Test that light icons are generated', () {
+    IMaterialIconLightData icon = const IMaterialIconLightData(0xe900);
+    expect(icon.codePoint, 0xe900);
+  });
+
   test('Test some icons', () {
     expect(IMaterialProRegular.Alert.codePoint, 0xe901);
     expect(IMaterialProLight.Alert.codePoint, 0xe901);
