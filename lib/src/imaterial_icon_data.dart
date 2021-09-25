9da2d8c9e4ae4175718cbb3647c79aa56cded97c
Mastersam <abadasamuelosp@gmail.com>
2021-09-25 02:20:20 -0700

    :art: code formatting
    code formatting

--- lib/src/imaterial_icon_data.dart
+++ lib/src/imaterial_icon_data.dart
@@ -4,10 +4,14 @@ import "package:flutter/widgets.dart";
 
 class IMaterialIconRegularData extends IconData {
   const IMaterialIconRegularData(int codePoint)
-      : super(codePoint, fontFamily: "iMaterialPro-regular", fontPackage: "imaterial_pro_flutter");
+      : super(codePoint,
+            fontFamily: "iMaterialPro-regular",
+            fontPackage: "imaterial_pro_flutter");
 }
 
 class IMaterialIconLightData extends IconData {
   const IMaterialIconLightData(int codePoint)
-      : super(codePoint, fontFamily: "iMaterialPro-light", fontPackage: "imaterial_pro_flutter");
+      : super(codePoint,
+            fontFamily: "iMaterialPro-light",
+            fontPackage: "imaterial_pro_flutter");
 }
